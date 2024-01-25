860eb2b911916e54eb72d82bb2c3599819f9859c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 21:49:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- js/jkcss.js
+++ js/jkcss.js
@@ -1,11 +1,11 @@
-window.onload = function(){
+
   const jkmodebtn = document.querySelector('#jkbtnPopup');
   const jkmodal = document.querySelector('#modalJK');
   const closeModel = document.querySelector('#closeModel');
 
   jkmodebtn.addEventListener('click', ModelJKOpen);
   closeModel.addEventListener('click', ModeljkClosebtn);
-  window.addEventListener('click', OutsideJKModel)
+  window.addEventListener('click', OutsideJKModel);
 
   function ModelJKOpen(){
     jkmodal.style.display = 'block';
@@ -14,5 +14,3 @@ window.onload = function(){
   function ModeljkClosebtn(){
     jkmodal.style.display = 'none';
   }
-  
-}
