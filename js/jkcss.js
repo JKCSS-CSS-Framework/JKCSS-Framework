2be113951058a796ffb685887b2a5baf052e38fb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-30 04:51:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+VinuraPathirana@users.noreply.github.com>

--- js/jkcss.js
+++ js/jkcss.js
@@ -6,5 +6,23 @@ window.onload = function(){
   document.getElementsByClassName('close')[0].addEventListener('click', function() {
     document.getElementById('modalJK').style.display = 'none';
   });
+
+  var openModals = document.querySelectorAll(".modal-open");
+
+  openModals.forEach(function(btn) {
+    btn.onclick = function() {
+      var modal = btn.getAttribute("data-modal");
+      document.getElementById(modal).style.display = "block";
+    };
+  });
+  
+  var closebtn = document.querySelectorAll('.close-btn');
+  
+  closebtn.forEach(function(btn){
+    btn.onclick = function(){
+      var modal = (btn.closest(".modal").style.display = "none");
+    }
+  });
+
 }
   
